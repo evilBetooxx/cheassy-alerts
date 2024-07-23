1ed70c2266be16a154c38c7edf316be5be0bfb68
evilBetooxx <albertof4in@gmail.com>
2024-07-23 13:40:23 -0600

    terminado

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "PREFLIGHT"],
     credentials: true,
   })
